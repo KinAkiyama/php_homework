770bb6d4158014a454cdc47c094f7fd5e9e3f7d4
Bakhar Ilya <artemgray22@gmail.com>
2024-02-20 20:49:18 +0300

    Lesson 9 HW done

--- index.php
+++ index.php
@@ -1,156 +1,94 @@
 <?php
 
-$str = "Я люблю обучаться php!";
+//1)удалить из массива все подстроки длина которых больше 3-х
+$initialArr = [1,22,333,4444,22,5555,1];
 
-//1)привести все слова к верхнему регистру
-echo "1) ". mb_strtoupper($str). "<br>"; //mbm_strtoupper конвертирует строку в верхний регистр
-// со специальными символами, кириллица, видимо, тоже спец. символы ¯\_(ツ)_/¯
+$changedArr = preg_replace('/\S{4,}/',"",$initialArr);
 
-//2)написать функцию, которая принимает две строки и заменяет одно слово другим
-function changeTheString($string, $word){
-    return preg_replace('/php/', $word, $string);
+function checkEmptyElement ($array){
+    for ($i=0;$i<sizeof($array)+1;$i++){
+        if ($array[$i] == ""){
+            unset($array[$i]);
+        }
+    }
+    return $array;
 }
 
-echo "2) ". changeTheString($str, "всему"). "<br>";
+echo "1.1) Решение через regex<br>";
+print_r(checkEmptyElement($changedArr));
 
-//3)посчитать количество слов в исходной строке
-$arrOfStr = explode(' ',$str);
+function replaceLargeElement($initialArr){
+    foreach ($initialArr as $key=>$value){
+        if (strlen((string)$value)>3){
+            unset($initialArr[$key]);
+        }
+    }   
+    return $initialArr;
+}
 
-echo "3) ". count($arrOfStr). "<br>";
- 
-//4)почитать количество всех символов строки
-echo "4) ".mb_strlen($str). "<br><br>";
+echo "<br>1.2) Решение через foreach <br>";
+print_r(replaceLargeElement($initialArr));
 
+//P.s я посмотрел время выполнения обеих функций и вторая более быстрая, поэтому более предпочтительная
 
+//2)преобразовать данную дату в массив типа год, месяц, день
+$initialData = "2025-12-31";
 
-echo "Задание c массивами <br>";
-
-// function output($anonymousFunction) {
-//     echo "1) ";
-//     print_r($arr);
-//     echo "<br>";
-// }
-
-$arr = [
-    "name" => "Fred",
-    "remove" => "True",
-    "additional_params" => [
-        "is_married" => false,
-        "country" => "France",
-        "born" => "10.09.1982"
-    ]
-];
-
-//1)удалить второй эллемент массива
-unset($arr['remove']);
-echo "1) ";
-print_r($arr);
-echo "<br>";
-
-//2)найти возраст пользователя
-echo "2) ". (date_diff(date_create($arr["additional_params"]["born"]),date_create())->y). "<br>";
-
-//3)отсортировать массив по ключу
-ksort($arr);
-echo "3) ";
-print_r($arr);
-echo "<br>";
-
-//4)добавить информацию о стране
-$country = [
-    "country" =>[
-        "countryName" => "France",
-        "capital" => "Paris",
-        "timeZone" => 1
-        ]
-];
-
-//информацию о стране можно получить и из функций, но там нужны базы данных или
-//нужно будет использовать много функций, потому что ни одна из них не возвращает
-//всю нужную информацию
-
-echo "4) ";
-print_r(array_replace($arr["additional_params"], $country));
-echo "<br>";
-
-//5)объеденить текущий массив с массивом child
-$child = ["child" => null];
-echo "5) ";
-print_r(array_merge($arr,$child));
-echo "<br>";
-
-//6)используя дату рождения превратить её в массив
 $date = [
-    "born" =>[
-        "day" => explode('.',$arr["additional_params"]["born"])[0],
-        "mounth" => explode('.',$arr["additional_params"]["born"])[1],
-        "year" => explode('.',$arr["additional_params"]["born"])[2]
-    ]
+    "year" => explode('-',$initialData)[0],
+    "month" => explode('-',$initialData)[1],
+    "day" => explode('-',$initialData)[2]
 ];
 
-echo "6) ";
-print_r(array_replace($arr["additional_params"], $date));
+echo "<br><br>2) Решение через explode <br>";
+var_dump($date);
+
+//Я не придумал как это можно сделать через цикл
+
+//3)функция для соритровки слов в строке в алфавитном порядке
+$initialString = "abc cd bsd";
+echo "<br><br>3) Начальная строка -> {$initialString}<br>";
+$initialString = explode(' ',$initialString);
+
+sort($initialString);
+echo "Отсортированная строка -> ". implode(' ',$initialString). "<br><br>";
+
+//4)написать функцию, которая принимает массив и элемент, а возвращает слдующий элемент
+$arr = [1,3,2,5,4];
+echo "4)";
+print_r($arr);
 echo "<br>";
 
-
-echo "<br><br>Доп. задание <br>";
-
-//1) вывести максимальное и минимальное число из даты рождения пользователя
-$born = explode('.',$arr["additional_params"]["born"]);
-sort($born);
-echo "1) <br>";
-echo "наименьшее число -> ". $born[0]. "<br>";
-echo "наибольшее число -> ". end($born). "<br>";
-echo "<br>";
-
-//2)отсортировать числа из даты рождения по возрастанию 
-sort($born); //используем массив из предыдущего задания
-echo "2) ";
-print_r($born);
-echo "<br>";
-
-//3)определить знак зодиака пользователя и определить знак по китайскому гороскопу
-if (intval($date["born"]["day"]) >= 22 && intval($date["born"]["mounth"]) == 8 || intval($date["born"]["day"]) <= 23 && intval($date["born"]["mounth"]) == 9){
-    echo "3) Знак зодиака -> Дева <br>"; //тоже самое для других знаков, задаем рамки дат
+function returnNextElement ($array,$element){
+    if (checkExistence($array,$element)){
+        foreach ($array as $key=>$val){
+            if ($element == $val){
+                if ($key == sizeof($array)-1){
+                    echo "Элемент следующий за {$val} -> ". $array[0]. "<br>";
+                } else {
+                    echo "Элемент следующий за {$val} -> ". $array[$key+1]. "<br>";
+                }
+            }
+        }
+    } else {
+        echo "Элемента не существует!";
+    }
 }
 
-switch(intval($date["born"]["year"])){
-    case 1972:
-        echo " Знак зодиака по китайскому гороскопу -> Крыса <br>";
-        break;
-    case 1973:
-        echo " Знак зодиака по китайскому гороскопу -> Бык <br>";
-        break;
-    case 1974:
-        echo " Знак зодиака по китайскому гороскопу -> Тигр <br>";
-        break;
-    case 1975:
-        echo " Знак зодиака по китайскому гороскопу -> Заяц <br>";
-        break;
-    case 1976:
-        echo " Знак зодиака по китайскому гороскопу -> Дракон <br>";
-        break;
-    case 1977:
-        echo " Знак зодиака по китайскому гороскопу -> Змея <br>";
-        break;
-    case 1978:
-        echo " Знак зодиака по китайскому гороскопу -> Лошадь <br>";
-        break;
-    case 1979:
-        echo " Знак зодиака по китайскому гороскопу -> Овца <br>";
-        break;
-    case 1980:
-        echo " Знак зодиака по китайскому гороскопу -> Обезьяна <br>";
-        break;
-    case 1981:
-        echo " Знак зодиака по китайскому гороскопу -> Петух <br>";
-        break;
-    case 1982:
-        echo " Знак зодиака по китайскому гороскопу -> Собака <br>";
-        break;
-    case 1983:
-        echo " Знак зодиака по китайскому гороскопу -> Свинья <br>";
-        break;
+function checkExistence ($array, $element){
+    $existance = false;
+
+    foreach ($array as $key => $el){
+        if ($el == $element){
+            $existance = true;
+        }
+    }
+    return $existance;
 }
 
-//наверное, можно как-то по другому через какой-то цикл, но я не придумал как
+// У меея пролучилось так, что в двух функциях очень похожий код и в обеих функциях идет
+// перебор всего массива, что нехорошо с точки зрения произодительности, поэтому я был
+// бы очень рад узнать, что можно оптимизировать
+
+returnNextElement($arr, 3);
+returnNextElement($arr, 4);
